a77c4d79f7d8cf53318350d3ad3c15cd5d4f16ed
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2023-06-21 00:01:12 -0600

    commiting to get data from github

---
+++ scripts/links.js
@@ -0,0 +1,10 @@
+const baseURL = 'https://github.com/aj-et/wdd230';
+const linksURL = 'https://github.com/aj-et/wdd230/data/links.json';
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
