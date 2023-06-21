90e151cc7ca43b037dc6503053c54d253225f201
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2023-06-21 00:24:28 -0600

    added the week stuff

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,43 @@
 const baseURL = 'https://github.com/aj-et/wdd230';
 const linksURL = 'https://github.com/aj-et/wdd230/data/links.json';
+const linksPath = 'data/links.json'
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    try {
+        const response = await fetch(linksURL);
+        const data = await response.json();
+        displayLinks(data);
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+function displayLinks(weeks) {
+    const linksContainer = document.querySelector('.links-container');
+
+    weeks.forEach((week) => {
+        const weekDiv = document.createElement('div');
+        weekDiv.classList.add('week');
+
+        const weekHeader = document.createElement('h2');
+        weekHeader.textContent = week.week;
+
+        const linksList = document.createElement('ul');
+
+        week.links.forEach((link) => {
+            const listItem = document.createElement('li');
+            const linkAnchor = document.createElement('a');
+            linkAnchor.href = baseURL + link.url;
+            linkAnchor.textContent = link.title;
+            listItem.appendChild(linkAnchor);
+            linksList.appendChild(listItem);
+        });
+
+        weekDiv.appendChild(weekHeader);
+        weekDiv.appendChild(linksList);
+        linksContainer.appendChild(weekDiv);
+    });
+
 }
 
 getLinks();
